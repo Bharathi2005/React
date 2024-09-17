f458891fd66a12b58a5f94cd9f598b266de1bcf6
BHARATHI A K <bharathiak2005@gmail.com>
2024-09-17 09:56:04 +0530

    main

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Footer from "./components/Footer";
+import Navbar from './components/NavBar';
+
+const App = () => {
+  const appname = "Footer Data"
+
+  const data = {
+      Appname: "BHARATHIII",
+      Appdata: "MEEEE"
+  }
 
 export const App = () => {
   return (
     <>
-     <form className='w-[80%] h-[80%] flex flex-auto justify-center items-center'>
+     {/* <form className='w-[80%] h-[80%] flex flex-auto justify-center items-center'>
   <div className="space-y-12">
     <div className="border-b border-gray-900/10 pb-12">
       <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
@@ -173,9 +183,13 @@ export const App = () => {
     <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
     <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
-</form>
+</form> */}
+ <div className="h-screen w-screen flex flex-col justify-start items-center">
+                <Navbar data={data} />
+                {/* login */}
+                <Footer appdata={appname} />
+            </div>
 
-    </>
-  )
-}
-export default App
+</>
+)}
+export default App;
--- sample/src/main.jsx
+++ sample/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './assests/css/style.css'
+import './assets/css/style.css'
 createRoot(document.getElementById('root')).render(
-    <App />
-    
+  <App />
 )
